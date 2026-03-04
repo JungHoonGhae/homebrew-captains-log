3aa19249cf902c662635c5b81219976ae3891c7f
Lucas <42439321+JungHoonGhae@users.noreply.github.com>
2026-03-04 18:31:50 +0900

    Update formula to v0.1.0

--- Formula/captains-log.rb
+++ Formula/captains-log.rb
@@ -2,7 +2,7 @@ class CaptainsLog < Formula
   desc "Pirate-themed macOS menu bar app that gamifies dev velocity"
   homepage "https://github.com/JungHoonGhae/captains-log"
   url "https://github.com/JungHoonGhae/captains-log/releases/download/v0.1.0/captains-log-macos.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "fd9dcca8a7f14192a0f3f86e079fa10a0eba461a6ef885e164eafc553c43fb3b"
   license "MIT"
 
   depends_on :macos
